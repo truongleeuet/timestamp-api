1e0ac4a221047405276baa0f46b5e9d845972f5a
truonglee.uet <truonglee.uet@gmail.com>
2017-01-10 04:25:56 -0500

    done timestamp-api

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express')
-var app = express()
+var app = express();
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/:parameter', function (req, res) {
+  var para = req.params.parameter;
+  if (!moment(para, ["x", "MMMM D YYYY"]).isValid()) {
+    res.send('null');
+  } else {
+    res.json({
+      unix: moment(para , ["x", "MMMM D YYYY"]).unix(),
+      natural: moment(para, ["x", "MMMM D YYYY"]).format("MMMM D, YYYY")
+    })
+  }
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
